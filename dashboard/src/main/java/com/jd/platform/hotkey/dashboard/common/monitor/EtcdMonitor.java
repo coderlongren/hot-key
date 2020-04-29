30bc565e2decac421f54438a1f828951db80aaa9
wuweifeng10 <Wu@0396110>
2020-04-29 10:34:47 +0800

    etcdMonitor写的不对

--- dashboard/src/main/java/com/jd/platform/hotkey/dashboard/common/monitor/EtcdMonitor.java
+++ dashboard/src/main/java/com/jd/platform/hotkey/dashboard/common/monitor/EtcdMonitor.java
@@ -1,25 +1,26 @@
 package com.jd.platform.hotkey.dashboard.common.monitor;
 
-import cn.hutool.core.date.SystemClock;
 import com.alibaba.fastjson.JSON;
 import com.ibm.etcd.api.Event;
 import com.ibm.etcd.api.KeyValue;
 import com.ibm.etcd.client.kv.KvClient;
 import com.jd.platform.hotkey.common.configcenter.ConfigConstant;
 import com.jd.platform.hotkey.common.configcenter.IConfigCenter;
-import com.jd.platform.hotkey.dashboard.mapper.*;
-import com.jd.platform.hotkey.dashboard.model.*;
-import com.jd.platform.hotkey.dashboard.service.KeyService;
-import com.jd.platform.hotkey.dashboard.service.RuleService;
+import com.jd.platform.hotkey.dashboard.mapper.ChangeLogMapper;
+import com.jd.platform.hotkey.dashboard.mapper.KeyRecordMapper;
+import com.jd.platform.hotkey.dashboard.mapper.KeyTimelyMapper;
+import com.jd.platform.hotkey.dashboard.model.ChangeLog;
+import com.jd.platform.hotkey.dashboard.model.KeyRecord;
+import com.jd.platform.hotkey.dashboard.model.KeyTimely;
+import com.jd.platform.hotkey.dashboard.model.Worker;
 import com.jd.platform.hotkey.dashboard.service.WorkerService;
-import com.jd.platform.hotkey.dashboard.util.CommonUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
 import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.util.Date;
-import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -47,35 +48,34 @@ public class EtcdMonitor {
 
     @PostConstruct
     public void watchHotKey() {
-        System.out.println("======== watchHotKey =======");
         CompletableFuture.runAsync(() -> {
             KvClient.WatchIterator watchIterator = configCenter.watchPrefix(ConfigConstant.hotKeyPath);
             while (watchIterator.hasNext()) {
-                System.out.println("======== watchHotKey =======");
                 Event event = event(watchIterator);
                 KeyValue kv = event.getKv();
-                System.err.println(event.getKv());
                 Event.EventType eventType = event.getType();
-                System.out.println(eventType.toString());
                 String k = kv.getKey().toStringUtf8();
                 String v = kv.getValue().toStringUtf8();
                 System.out.println(JSON.toJSONString(eventType));
-                System.out.println("k-> "+k);
-                System.out.println("v-> "+v);
+                System.out.println("k-> " + k);
+                System.out.println("v-> " + v);
                 long ttl = configCenter.timeToLive(kv.getLease());
                 String appKey = k.replace(ConfigConstant.hotKeyPath, "");
                 String[] arr = appKey.split("/");
-                System.out.println("arr-> "+JSON.toJSONString(arr));
-                if(eventType.equals(Event.EventType.PUT)){
-                    if(v.equals("1")){
-                        keyTimelyMapper.insertSelective(new KeyTimely(arr[1],v,arr[0],ttl));
-                    }else if(v.equals("UPDATE")){
-                        keyTimelyMapper.updateByKey(new KeyTimely(arr[1],ttl));
+                if (eventType.equals(Event.EventType.PUT)) {
+                    if ("1".equals(v)) {
+                        keyTimelyMapper.insertSelective(new KeyTimely(arr[1], v, arr[0], ttl));
+                        keyRecordMapper.insertSelective(new KeyRecord(arr[1], v, arr[0], ttl, "SYSTEM", eventType.getNumber(), new Date()));
+                    } else {
+                        //手工加的
+                        keyTimelyMapper.updateByKey(new KeyTimely(arr[1], ttl));
+                        keyRecordMapper.insertSelective(new KeyRecord(arr[1], v, arr[0], ttl, "HAND", eventType.getNumber(), new Date()));
                     }
-                }else if(eventType.equals(Event.EventType.DELETE)){
+                } else if (eventType.equals(Event.EventType.DELETE)) {
                     keyTimelyMapper.deleteByKey(k);
+                    keyRecordMapper.insertSelective(new KeyRecord(arr[1], v, arr[0], 0L, "SYSTEM", eventType.getNumber(), new Date()));
                 }
-                keyRecordMapper.insertSelective(new KeyRecord(arr[1],v,arr[0],ttl, "SYSTEM", eventType.getNumber(),new Date()));
+
             }
         });
 
@@ -93,19 +93,20 @@ public class EtcdMonitor {
                 KeyValue kv = event.getKv();
                 System.err.println(event.getKv());
                 Event.EventType eventType = event.getType();
-                System.out.println(eventType.toString());;
+                System.out.println(eventType.toString());
+                ;
                 String k = kv.getKey().toStringUtf8();
                 String v = kv.getValue().toStringUtf8();
                 System.out.println(JSON.toJSONString(eventType));
                 long version = kv.getModRevision();
-                System.out.println("k-> "+k);
-                System.out.println("v-> "+v);
-                String app = k.replace(ConfigConstant.rulePath,"");
-                String uuid = app+"_"+version;
-                if(eventType.equals(Event.EventType.PUT)){
-                    logMapper.insertSelective(new ChangeLog(app,1,"",v,"SYSTEM",app,uuid));
-                }else if(eventType.equals(Event.EventType.DELETE)){
-                    logMapper.insertSelective(new ChangeLog(app,1,v,"","SYSTEM",app,uuid));
+                System.out.println("k-> " + k);
+                System.out.println("v-> " + v);
+                String app = k.replace(ConfigConstant.rulePath, "");
+                String uuid = app + "_" + version;
+                if (eventType.equals(Event.EventType.PUT)) {
+                    logMapper.insertSelective(new ChangeLog(app, 1, "", v, "SYSTEM", app, uuid));
+                } else if (eventType.equals(Event.EventType.DELETE)) {
+                    logMapper.insertSelective(new ChangeLog(app, 1, v, "", "SYSTEM", app, uuid));
                 }
             }
         });
@@ -128,12 +129,12 @@ public class EtcdMonitor {
                 String k = kv.getKey().toStringUtf8();
                 String v = kv.getValue().toStringUtf8();
                 System.out.println(JSON.toJSONString(eventType));
-                System.out.println("k-> "+k);
-                System.out.println("v-> "+v);
-                Worker worker = new Worker(k,v);
-                if(eventType.equals(Event.EventType.PUT)){
+                System.out.println("k-> " + k);
+                System.out.println("v-> " + v);
+                Worker worker = new Worker(k, v);
+                if (eventType.equals(Event.EventType.PUT)) {
                     workerService.insertWorkerBySys(worker);
-                }else if(eventType.equals(Event.EventType.DELETE)){
+                } else if (eventType.equals(Event.EventType.DELETE)) {
                     worker.setState(0);
                     workerService.updateWorker(worker);
                 }
@@ -142,7 +143,7 @@ public class EtcdMonitor {
     }
 
 
-    private Event event(KvClient.WatchIterator watchIterator){
+    private Event event(KvClient.WatchIterator watchIterator) {
         return watchIterator.next().getEvents().get(0);
     }
 
