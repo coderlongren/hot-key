53495b918dccf6165dce3dbd32d469a9314d0436
liyunfeng31 <liyunfeng31@jd.com>
2020-05-11 23:59:22 +0800

    SQL

--- dashboard/src/main/resources/db.sql
+++ dashboard/src/main/resources/db.sql
@@ -51,7 +51,6 @@ CREATE TABLE `hk_key_rule`  (
   `update_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '修改时间',
   `version` int(11) NOT NULL DEFAULT 0 COMMENT '数据版本',
   PRIMARY KEY (`id`) USING BTREE,
-  UNIQUE INDEX `uniq_key`(`key_name`) USING BTREE COMMENT '唯一索引'
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Compact;
 
 
@@ -65,14 +64,10 @@ CREATE TABLE `hk_key_record`  (
   `source` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '来源',
   `type` int(11) NOT NULL DEFAULT 1 COMMENT '记录类型：1put；2del; -1unkonw',
   `create_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `uuid` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '防重ID',
   PRIMARY KEY (`id`) USING BTREE
+  UNIQUE INDEX `uniq_key`(`uuid`) USING BTREE COMMENT '唯一索引'
 ) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Compact;
-
--- ----------------------------
--- Records of hk_key_record
--- ----------------------------
-INSERT INTO `hk_key_record` VALUES (1, '123', '123', '123123124233val', 123, 'lyf', 1, '2020-04-26 09:26:24');
-INSERT INTO `hk_key_record` VALUES (2, '123', '123', '123123124233val', 123, 'lyf', 1, '2020-04-26 09:28:13');
 
 
 DROP TABLE IF EXISTS `hk_change_log`;
@@ -85,7 +80,9 @@ CREATE TABLE `hk_change_log`  (
   `app_name` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '数据所属APP',
   `update_user` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '修改人',
   `create_time` datetime(0) NOT NULL ON UPDATE CURRENT_TIMESTAMP(0) COMMENT '创建时间',
+  `uuid` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '防重ID',
   PRIMARY KEY (`id`) USING BTREE
+  UNIQUE INDEX `uniq_key`(`uuid`) USING BTREE COMMENT '唯一索引'
 ) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Compact;
 
 
@@ -95,20 +92,16 @@ CREATE TABLE `hk_key_timely`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `key_name` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT 'key',
   `val` varchar(2048) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT 'value',
-  `parent_key` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '父级KEY',
+  `uuid` varchar(128) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '防重ID',
   `app_name` varchar(64) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL DEFAULT '' COMMENT '所属appName',
   `duration` int(11) NOT NULL DEFAULT 0 COMMENT '缓存时间',
   `create_time` bigint(20) NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`) USING BTREE
+  UNIQUE INDEX `uniq_key`(`uuid`) USING BTREE COMMENT '唯一索引'
 ) ENGINE = InnoDB AUTO_INCREMENT = 8 CHARACTER SET = utf8 COLLATE = utf8_bin ROW_FORMAT = Compact;
 
 -- ----------------------------
 -- Records of hk_key_timely
 -- ----------------------------
-INSERT INTO `hk_key_timely` VALUES (1, '/jd/hotkeys/test/testk1', 'testv1', '/jd/hotkeys/test/', 'test', 60000, 1212121);
-INSERT INTO `hk_key_timely` VALUES (2, '/jd/hotkeys/test/testk2', 'testv2', '/jd/hotkeys/test/', 'test', 10000, 123443345);
-INSERT INTO `hk_key_timely` VALUES (3, '/jd/hotkeys/test/', '', '/jd/hotkeys/', 'test', 2000, 12334343346);
-INSERT INTO `hk_key_timely` VALUES (4, '/jd/hotkeys/', '', 'jd/', '', 150000, 12342343543);
-INSERT INTO `hk_key_timely` VALUES (5, '/jd/hotkeys/cart/', '', '/jd/hotkeys/', 'cart', 250000, 5767676322);
-INSERT INTO `hk_key_timely` VALUES (6, '/jd/hotkeys/cart/testk3', 'testv3', '/jd/hotkeys/cart/', 'cart', 500000, 124546666);
-INSERT INTO `hk_key_timely` VALUES (7, '123', '123123124233val', '', '123', 123, 1587893292779);
+INSERT INTO `hk_key_timely` VALUES (1, 'testk1', 'testv1', '/jd/hotkeys/test/', 'test', 60000, 1212121);
+INSERT INTO `hk_key_timely` VALUES (2, 'testk2', 'testv2', '/jd/hotkeys/test/', 'test', 10000, 123443345);
