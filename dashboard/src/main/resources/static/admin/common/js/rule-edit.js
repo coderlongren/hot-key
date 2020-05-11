07116b60a9d5d85c34f3eb027a0814877a3dbe16
liyunfeng31 <liyunfeng31@jd.com>
2020-05-11 15:51:46 +0800

    fix page limit

--- dashboard/src/main/resources/static/admin/common/js/rule-edit.js
+++ dashboard/src/main/resources/static/admin/common/js/rule-edit.js
@@ -17,24 +17,15 @@ function edit() {
 		},
 		async : false,
 		error : function(response,status,xhr) {
-			alert("response: "+response);	//服务器返回的信息
-			alert("status: "+status);	//服务器返回的信息
-			alert("xhr.status: "+xhr.status);	//状态码,   要看其他的直接 输出 xhr 就行
-			alert("Headers: "+xhr.getAllResponseHeaders()); //响应头部
+
 			$.modal.alertError("系统错误111111"+response);
 		},
 		fail : function(response,status,xhr) {
-			alert("failresponse: "+response);	//服务器返回的信息
-			alert("failstatus: "+status);	//服务器返回的信息
-			alert("failxhr.status: "+xhr.status);	//状态码,   要看其他的直接 输出 xhr 就行
-			alert("failHeaders: "+xhr.getAllResponseHeaders()); //响应头部
+
 			$.modal.alertError("系统错误"+response);
 		},
 		success : function(response,status,xhr,data) {
-			alert(response);	//服务器返回的信息
-			alert(status);	//服务器返回的信息
-			alert(xhr.status);	//状态码,   要看其他的直接 输出 xhr 就行
-			alert(xhr.getAllResponseHeaders()); //响应头部
+
 			$.operate.saveSuccess(data);
 		}
 	});
