49a1c2e0329e667e3c104b32bc28d7bdcd561587
wuweifeng10 <Wu@0396110>
2020-06-08 10:47:21 +0800

    不批量入库了

--- dashboard/src/main/java/com/jd/platform/hotkey/dashboard/common/monitor/DataHandler.java
+++ dashboard/src/main/java/com/jd/platform/hotkey/dashboard/common/monitor/DataHandler.java
@@ -16,21 +16,17 @@ import com.jd.platform.hotkey.dashboard.util.RuleUtil;
 import com.jd.platform.hotkey.dashboard.util.TwoTuple;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.dao.DuplicateKeyException;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import javax.annotation.Resource;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Objects;
-import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.LinkedBlockingQueue;
 
 @Component
 public class DataHandler {
@@ -44,116 +40,55 @@ public class DataHandler {
     @Resource
     private StatisticsMapper statisticsMapper;
 
-
-    @Value("${pool.size:4}")
-    private String poolSize = "4";
-
     /**
      * 队列
      */
-    private ConcurrentLinkedQueue<EventWrapper> queue = new ConcurrentLinkedQueue<>();
-
-    /**
-     * 4个线程用来入库
-     */
-    private Executor executor = Executors.newFixedThreadPool(Integer.parseInt(poolSize));
+    private BlockingQueue<EventWrapper> queue = new LinkedBlockingQueue<>();
 
     /**
      * 入队
      */
     public void offer(EventWrapper eventWrapper) {
-        queue.offer(eventWrapper);
-    }
-
-    /**
-     * 每1秒批量保存一次.每秒最多处理1万个，每1千个放一个线程里
-     */
-    @Scheduled(fixedRate = 1000)
-    public void batchInsertRecords() {
         try {
-            List<KeyRecord> keyRecords = new ArrayList<>(10000);
-            List<KeyTimely> keyTimelies = new ArrayList<>(10000);
-            if (queue.isEmpty()) {
-                return;
-            }
-            //TODO 此处可以判断实际数量，不用add到10000个null，但我懒的写
-//            queue.size()
-
-            for (int i = 0; i < 10000; i++) {
-                if (!queue.isEmpty()) {
-                    TwoTuple<KeyTimely, KeyRecord> twoTuple = handHotKey(queue.poll());
-                    keyRecords.add(twoTuple.getSecond());
-                    keyTimelies.add(twoTuple.getFirst());
-                } else {
-                    keyRecords.add(null);
-                    keyTimelies.add(null);
-                }
-            }
-
-            for (int i = 0; i < 10; i++) {
-                List<KeyRecord> tempRecords = keyRecords.subList(1000 * i, 1000 * (i + 1));
-                List<KeyTimely> tempTimelies = keyTimelies.subList(1000 * i, 1000 * (i + 1));
-                executor.execute(() -> batchInsertRecord(tempRecords));
-                executor.execute(() -> batchTimely(tempTimelies));
-            }
-        } catch (Throwable t) {
-            t.printStackTrace();
-            log.info(t.getMessage());
-        }
-
-    }
-
-
-    private void batchInsertRecord(List<KeyRecord> keyRecords) {
-        List<KeyRecord> records = keyRecords.stream().filter(Objects::nonNull).collect(Collectors.toList());
-        int row = 0;
-        if (records.size() > 0) {
-            try {
-                row = keyRecordMapper.batchInsert(records);
-            } catch (DuplicateKeyException e) {
-                // log.warn("DuplicateKey");
-            }
-            log.info("keyRecords insert rows " + row);
-        }
-    }
-
-    /**
-     * 批量插入、删除实时热点
-     */
-    private void batchTimely(List<KeyTimely> keyTimelies) {
-        List<KeyTimely> insertList = new ArrayList<>();
-        List<KeyTimely> deleteList = new ArrayList<>();
-        for (KeyTimely keyTimely : keyTimelies) {
-            if (keyTimely == null) {
-                continue;
-            }
-            if (keyTimely.getUuid() == null) {
-                deleteList.add(keyTimely);
-            } else {
-                insertList.add(keyTimely);
-            }
-        }
-
-        try {
-            int row;
-            if (insertList.size() > 0) {
-                row = keyTimelyMapper.batchInsert(insertList);
-                log.info("batch insert keyTimely : " + row);
-            }
-        } catch (DuplicateKeyException e) {
-            //有重复的uuid，说明被别的插入过了
-        }
-
-        try {
-            if (deleteList.size() > 0) {
-                //改成批量删除
-                keyTimelyMapper.batchDeleted(deleteList);
-            }
-        } catch (Exception e) {
+            queue.put(eventWrapper);
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
+    }
+
+    @PostConstruct
+    public void insertRecords() {
+        CompletableFuture.runAsync(() -> {
+            while (true) {
+                TwoTuple<KeyTimely, KeyRecord> twoTuple = null;
+                try {
+                    twoTuple = handHotKey(queue.take());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                KeyRecord keyRecord = twoTuple.getSecond();
+                KeyTimely keyTimely = twoTuple.getFirst();
+
+                if (keyTimely.getUuid() == null) {
+                    keyTimelyMapper.deleteByKeyAndApp(keyTimely.getKey(), keyTimely.getAppName());
+                } else {
+                    try {
+                        keyTimelyMapper.insertSelective(keyTimely);
+                    } catch (Exception e) {
+                        log.info("insert timely error");
+                    }
+                }
+
+                if (keyRecord == null) {
+                    return;
+                }
+                //插入记录
+                keyRecordMapper.insertSelective(keyRecord);
+            }
+        });
 
     }
+
 
     /**
      * 处理热点key和记录
@@ -202,12 +137,12 @@ public class DataHandler {
             if (records.size() == 0) {
                 return;
             }
-            records.forEach(x->{
+            records.forEach(x -> {
                 x.setBizType(1);
                 x.setCreateTime(nowTime);
                 x.setDays(day);
                 x.setHours(hour);
-                x.setUuid(1+"_"+x.getKeyName()+"_"+hour);
+                x.setUuid(1 + "_" + x.getKeyName() + "_" + hour);
             });
             int row = statisticsMapper.batchInsert(records);
             log.info("定时统计热点记录时间：{}, 影响行数：{}", now.toString(), row);
@@ -231,12 +166,12 @@ public class DataHandler {
             if (records.size() == 0) {
                 return;
             }
-            records.forEach(x->{
+            records.forEach(x -> {
                 x.setBizType(1);
                 x.setCreateTime(nowTime);
                 x.setDays(day);
                 x.setHours(hour);
-                x.setUuid(1+"_"+x.getKeyName()+"_"+hour);
+                x.setUuid(1 + "_" + x.getKeyName() + "_" + hour);
             });
             int row = statisticsMapper.batchInsert(records);
             log.info("定时统计热点记录时间：{}, 影响行数：{}", now.toString(), row);
