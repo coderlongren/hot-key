6c9b13555f2f9fab2a5c995e43cfcff2b25cbf88
wuweifeng10 <Wu@0396110>
2020-06-11 10:32:18 +0800

    1

--- sample/src/main/java/com/jd/platform/sample/Cache.java
+++ sample/src/main/java/com/jd/platform/sample/Cache.java
@@ -20,14 +20,54 @@ public class Cache {
         return redisTemplate.opsForValue().get(key);
     }
 
+    //最佳实践：
+    //
+    //1 判断用户是否是刷子
+    //
+    //        if (JdHotKeyStore.isHotKey(“pin__” + thePin)) {
+    //            //限流他，do your job
+    //        }
+    //2 判断商品id是否是热点
+    //
+    //
+    //
+    //     Object skuInfo = JdHotKeyStore.getValue("skuId__" + skuId);
+    //           if(skuInfo == null) {
+    //
+    //         JdHotKeyStore.smartSet("skuId__" + skuId, theSkuInfo);
+    //           } else {
+    //
+    //                  //使用缓存好的value即可
+    //
+    //            }
+    //
+    //   或者这样：
+    //
+    //
+    //
+    //        if (JdHotKeyStore.isHotKey(key)) {
+    //                           //注意是get，不是getValue。getValue会获取并上报，get是纯粹的本地获取
+    //
+    //            Object skuInfo = JdHotKeyStore.get("skuId__" + skuId);
+    //                          if(skuInfo == null) {
+    //
+    //                JdHotKeyStore.smartSet("skuId__" + skuId, theSkuInfo);
+    //                          } else {
+    //
+    //                                  //使用缓存好的value即可
+    //
+    //                          }
+    //
+    //        }
+
     public String get(String key) {
         //如果已经缓存过了
-        if (JdHotKeyStore.isValueCached(key)) {
+        if (JdHotKeyStore.getValue(key) != null) {
             System.out.println("1");
             return JdHotKeyStore.getValue(key).toString();
         } else {
             String value = getFromRedis(key);
-            JdHotKeyStore.setValue(key, value);
+            JdHotKeyStore.smartSet(key, value);
             return value;
         }
     }
