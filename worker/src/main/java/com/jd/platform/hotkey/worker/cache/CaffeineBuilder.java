ba43383028ea07c15bc2800bbea6adb7d1d3e9f1
wuweifeng10 <Wu@0396110>
2020-06-17 16:49:39 +0800

    caffeine的过期线程池设置

--- worker/src/main/java/com/jd/platform/hotkey/worker/cache/CaffeineBuilder.java
+++ worker/src/main/java/com/jd/platform/hotkey/worker/cache/CaffeineBuilder.java
@@ -3,6 +3,8 @@ package com.jd.platform.hotkey.worker.cache;
 import com.github.benmanes.caffeine.cache.Cache;
 import com.github.benmanes.caffeine.cache.Caffeine;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -10,6 +12,7 @@ import java.util.concurrent.TimeUnit;
  * @version 1.0
  */
 public class CaffeineBuilder {
+    private static ExecutorService executorService = Executors.newFixedThreadPool(4);
     /**
      * 构建所有来的要缓存的key cache
      */
@@ -17,8 +20,9 @@ public class CaffeineBuilder {
         //老版本jdk1.8.0_20之前，caffeine默认的forkJoinPool在及其密集的淘汰过期时，会有forkJoinPool报错。建议用新版jdk
         return Caffeine.newBuilder()
                 .initialCapacity(8192)//初始大小
-                .maximumSize(5000000)//最大数量
+                .maximumSize(20000000)//最大数量
                 .expireAfterWrite(1, TimeUnit.MINUTES)//过期时间
+                .executor(executorService)
                 .softValues()
                 .build();
     }
@@ -29,14 +33,26 @@ public class CaffeineBuilder {
     public static Cache<String, Object> buildRecentHotKeyCache() {
         return Caffeine.newBuilder()
                 .initialCapacity(256)//初始大小
-                .maximumSize(50000)//最大数量
+                .maximumSize(500000)//最大数量
                 .expireAfterWrite(10, TimeUnit.SECONDS)//过期时间
+                .executor(executorService)
                 .softValues()
                 .build();
     }
 
     public static void main(String[] args) {
-        System.out.println(System.getProperty("java.version"));
+//        Cache cache = buildAllKeyCache();
+//        //开启上传worker信息
+//        ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+//        scheduledExecutorService.scheduleAtFixedRate(() -> {
+//            System.out.println(cache.asMap().size());
+//
+//        }, 0, 5, TimeUnit.SECONDS);
+//
+//        long i = 0;
+//        while (true) {
+//            cache.put(i++ + "", i);
+//        }
     }
 
 }
